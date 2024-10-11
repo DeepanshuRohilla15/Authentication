109fe1a5d0912349c1b2305f6e40ee5577c956ae
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-11 22:46:27 +0530

    Using JsonWebTokens

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-const e = require("express");
-const express = require("express")
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = "iamthebest"
 
 const app = express()
 app.use(express.json())
 
 const users = [];
-
-function generateToken(){
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-        'A', 'B', 'C', 'D', 'E', 'F', 'J', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4',
-        '5', '6', '7', '8', '9'];
-
-    let token = ""
-    for(let i = 0; i < 32; i++){
-        token = token + options[Math.floor(Math.random()* options.length)];
-    }
-    return token;
-}
 
 app.post("/signup", function(req, res){
     const username = req.body.username;
@@ -47,8 +34,12 @@ app.post("/signin", function(req, res){
     }
 
     if(foundUser){
-        const token = generateToken();
-        foundUser.token = token
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET);
+
+        // foundUser.token = token
+
         res.json({
             token: token
         })
@@ -62,11 +53,15 @@ app.post("/signin", function(req, res){
 
 
 app.get("/me", function(req, res){
-    const token = req.headers.token;
+    const token = req.headers.token;  // jwt
+    const decodedInformation = jwt.verify(token, JWT_SECRET); // {username: deepanshu}
+    const username = decodedInformation.username;
+
+
     let foundUser = null;
 
     for(let i = 0; i < users.length; i++){
-        if(users[i].token == token){
+        if(users[i].username == username){
             foundUser = users[i];
         }
     }
